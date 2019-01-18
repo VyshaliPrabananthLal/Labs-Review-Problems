3a0923f96d9b1711b693bb9cc8f8876bbd322dec
HP <HP@10.0.0.78>
2019-01-17 23:25:58 -0500

    Second Commit

--- Bouncy Bubble Sort Lab/src/bouncysort/BBSort.java
+++ Bouncy Bubble Sort Lab/src/bouncysort/BBSort.java
@@ -13,6 +13,12 @@ public class BBSort
 		int n = sc.nextInt();
 		Integer[] sortArray = new Integer[n];
 		System.out.println("Enter the elements that you will sort");
+		
+		// instead of making passes through a list that starts at the beginning and runs through to the end, you should reverse the direction of each pass.
+		// That is, if the first pass starts at the beginning of the list and runs through to the end, 
+		// the second pass would run from the end of the list back to the beginning, 
+		// and then the third pass would start at the beginning again.
+		
 		for(int i = 0; i < n; i++)
 		{
 			sortArray[i] = sc.nextInt();
